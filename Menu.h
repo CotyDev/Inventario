b3d63e9681a0065f050adf0fd8f81b8080ad7c2b
CotyDev <cotydev@gmail.com>
2017-04-10 22:10:06 -0400

    solo esta creado el esquema, falta programar el switch y la salida de cada opcion.

---
+++ Menu.h
@@ -0,0 +1,26 @@
+#ifndef Menu
+#define Menu
+
+//esquema del menu
+
+
+cout<<"MENU \n";
+
+cout<<"1. Opciones de articulo\n 2. Opciones de proveedor \n 3.Opciones de plan de medida\n";
+
+cout<<"4. Opciones de categoria \n 5. Opciones lista de precios\n 6. Reportes\n";
+
+int opc=0;
+
+
+
+switch(opc){
+	
+	case 1:
+		//logica del comportamiento del menu
+		cout<<"klk";
+	
+}
+
+
+#endif
