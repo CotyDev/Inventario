eb43f90b5bb1f79d36c594af99af916b00b15841
CotyDev <cotydev@gmail.com>
2017-04-16 12:13:13 -0400

    actualizacion de proveedores.h

--- Proveedores.h
+++ Proveedores.h
@@ -1,26 +1,78 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "access.h"
+
 #ifndef Proveedores
 #define proveedores
 
-//NOTA: falta agregar los parametros para completar las funciones que estaran en el main
-class provedores{
+
+
+const string uproveedores = "BD/proveedores.txt" //ubicacion del archivo del proveedor
+
+using namespace std;
+
+class data_proveedores{
 	public:
 		
-	char PRO_id[5];
-	char PRO_desc[20];
-	char LIST_id[5];
-	char PRO_email[20];
-	int PRO_tel1;
-	int PRO_tel2;
+	string prov_id;
+	string prov_desc;
+	string list_id;
+	string prov_email;
+	int prov_tel1;
+	int prov_tel2;
+	
+	public:
+					
+	
+	data_proveedores (string prov_id,
+	string prov_desc,
+	string list_id,
+	string prov_email,
+	int prov_tel1,
+	int prov_tel2,)	{								//iniciando metodo constructor
+		
+		this->list_id = list_id;
+		this->prov_desc = prov_desc;
+		this->prov_email = prov_email;
+		this->prov_id = prov_id;
+		this->prov_tel1 = prov_tel1;
+		this->prov_tel2 = prov_tel2;
+		
+			}	
+			
+			int i_proveedores(){
+				
+			
+				stringstream telefono;
+				string tel[2];
+				
+				telefono<< this.prov_tel1;
+				telefono>>tel[0];
+				telefono.clear();
+				
+				telefono<<this.prov_tel2;
+				telefono>>tel[1];
+				telefono.clear();
+				
+			string proveedores = this->list_id + "\t" + this->prov_desc + "\t" + this->prov_email +
+			"\t" + this.prov_id + "\t" + tel[0]	+ "\t" + tel[1] + "\n";
+			
+			insert(uproveedores,registro);
+				
+				
+				
+			}		
+		
+		
+		
+	
 	
 };
 
-FILE* PRO_file;
-char* PRO_file_name("proveedores.txt");
-  PRO_file = fopen (PRO_file_name,"w");
+
   
-  //completar con la variables y parametros que se usen en el main
-  
-  fclose(PRO_file);
+
   
   
 #endif
