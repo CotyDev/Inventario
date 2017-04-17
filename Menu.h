39717657f4f314a48cf3609508367d72cb7ea595
CotyDev <cotydev@gmail.com>
2017-04-17 19:25:10 -0400

    actualizacion del menu

--- Menu.h
+++ Menu.h
@@ -4,21 +4,47 @@
 //esquema del menu
 
 
-cout<<"MENU \n";
+cout<<"MENU \n"
 
-cout<<"1. Opciones de articulo\n 2. Opciones de proveedor \n 3.Opciones de plan de medida\n";
+<<"1. Opciones de articulo\n"
+<<"A1. Consultar \n"
+<<"A2. Insertar \n"
+ 
+<<"2. Opciones de proveedor \n"
+<<"P1. Consultar\n"
+<<"P2. Insertar"
+ 
+<<"3.Opciones de plan de medida\n"
+<<"U1. consultar"
+<<"U2. Insertar"
 
-cout<<"4. Opciones de categoria \n 5. Opciones lista de precios\n 6. Reportes\n";
+<<"4. Opciones de categoria \n"
+<<"C1. Consultar"
+<<"C2. Insertar"
+ 
+<<"5. Opciones lista de precios\n" 
+<<"L1. Consultar"
+<<"L2. Insertar"
+
+<<"6. Reportes\n"
+<<"R1. Reporte de conteo fisico"
+<<"R2. Reporte de variacion de existencias"
+<<"R3. Reporte de ajuste de inventario"
+<<"R4. Lista de articulos"
+<<"R5. Lista de proveedores"
+<<"R6. Lista de Listas de precios"
+<<"R7. Lista de Categorias"
+<<"R8. Lista de plan de unidades de medida"
 
 int opc=0;
 
 
 
-switch(opc){
+switch(opc){			//completar logica con funciones de inserccion y consulta
 	
 	case 1:
-		//logica del comportamiento del menu
-		cout<<"klk";
+		
+		cout<<"funciones de insertar";
 	
 }
 
