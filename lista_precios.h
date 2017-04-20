18ae5e0ab46902967ace850fef2d363d890d206b
CotyDev <cotydev@gmail.com>
2017-04-19 23:45:56 -0400

    Add files via upload

--- lista_precios.h
+++ lista_precios.h
@@ -1,23 +1,43 @@
-#ifndef Lista_precios
 #include <iostream>
 #include <string.h>
-#define Lista_precios
 #include "access.h"
-#define ulista C:\Users\ARTURO\Desktop\Inventario\Inventario\BD\Lista de precios //poner ruta que se requiera cuando se descargue
+
+#ifndef Lista_precios
+#define Lista_precios
+
+const string ulista_precios = "BD/articulo.txt";
 
 using namespace std;
-//NOTA: falta agregar los parametros para completar las funciones que estaran en el main
 
-class lista_precios{
+
+class data_lista_precios{
 	
 	public:
-	char list_id[5];
-	char list_desc[20];
+	string list_id;
+	string list_desc;
 	int list_moneda;
+	
+	public:
+		
+		data_lista_precios(string list_id,
+	string list_desc,
+	int list_moneda){
+		
+		this->list_id = list_id;
+		this->list_desc = list_desc;
+		this->list_moneda = list_moneda;
+		
+		
+	}
+	
+	
+	
 
 	int i_lista_precios(){
-		string lista = (string)this->list_desc + "\t"+ (string)this->list_id "\t" + (string)this->list_moneda;
 		
+		string registro = this->list_desc + "\t"+ this->list_id + "\t" + this->list_moneda + "\n";
+		
+		insert(ulista_precios,registro);
 		
 	}
 	
